3f0a4bffc6022467a0bcafa9173f3eef0cc16e18
evan <281406674@qq.com>
2018-07-26 10:21:25 +0800

    helloworld contract move to hello_world dir
    Signed-off-by: evan <281406674@qq.com>

--- test/contract_hello_world.test.go
+++ test/contract_hello_world.test.go
@@ -1,29 +1,64 @@
 package main
 
 import (
+	"context"
+	"crypto/ecdsa"
 	"fmt"
-	"log"
+	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	"medichain/contracts" // for demo
-	"DNA/core/store"
+	helloWorldContracts "medichain/contracts/hello_world" // for demo
 )
 
 func main() {
-	client, err := ethclient.Dial("https://rinkeby.infura.io")
+	client, err := ethclient.Dial("http://10.6.250.56:8545")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("dail http://10.6.250.56:8545 ===>", err)
+		return
 	}
 
-	address := common.HexToAddress("0x147B8eb97fD247D06C4006D269c90C1908Fb5D54")
-	contracts.new
-	//instance, err := store.NewStore(address, client)
+	privateKey, err := crypto.HexToECDSA("bcec428d5205abe0f0cc8a734083908d9eb8563e31f943d760786edf42ad67dd")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return
 	}
 
-	fmt.Println("contract is loaded")
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		fmt.Println("error casting public key to ECDSA")
+		return
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		fmt.Println("PendingNonceAt ===>", err)
+		return
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		fmt.Println("SuggestGasPrice ===>", err)
+		return
+	}
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	address, tx, instance, err := helloWorldContracts.DeployContracts(auth, client)
+	if err != nil {
+		fmt.Println("DeployContracts ===>", err)
+		return
+	}
+
+	fmt.Println(address.Hex())   // 0x147B8eb97fD247D06C4006D269c90C1908Fb5D54
+	fmt.Println(tx.Hash().Hex()) // 0xdae8ba5444eefdc99f4d45cd0c4f24056cba6a02cefbf78066ef9f4188ff7dc0
+
 	_ = instance
 }
--- test/wallet.test.go
+++ test/wallet.test.go
@@ -21,7 +21,7 @@ func main() {
 	}
 
 	privateKeyBytes := crypto.FromECDSA(privateKey)
-	fmt.Println("私钥", hexutil.Encode(privateKeyBytes)[2:]) // 0xfad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19
+	fmt.Println("私钥", len(hexutil.Encode(privateKeyBytes)[2:]), hexutil.Encode(privateKeyBytes)[2:]) // 0xfad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
@@ -30,7 +30,7 @@ func main() {
 	}
 
 	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
-	fmt.Println("公钥", hexutil.Encode(publicKeyBytes)[4:]) // 0x049a7df67f79246283fdc93af76d4f8cdd62c4886e8cd870944e817dd0b97934fdd7719d0810951e03418205868a5c1b40b192451367f28e0088dd75e15de40c05
+	fmt.Println("公钥", len(hexutil.Encode(publicKeyBytes)[4:]), hexutil.Encode(publicKeyBytes)[4:]) // 0x049a7df67f79246283fdc93af76d4f8cdd62c4886e8cd870944e817dd0b97934fdd7719d0810951e03418205868a5c1b40b192451367f28e0088dd75e15de40c05
 
 	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
 	fmt.Println("地址", address) // 0x96216849c49358B10257cb55b28eA603c874b05E
