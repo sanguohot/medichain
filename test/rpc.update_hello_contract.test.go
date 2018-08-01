28b102c8f29423104a1ea3e0553aa73e28f046e9
evan <281406674@qq.com>
2018-08-01 11:11:49 +0800

    modify test/rpc.update_hello_contract.test.go
    Signed-off-by: evan <281406674@qq.com>

--- test/rpc.update_hello_contract.test.go
+++ test/rpc.update_hello_contract.test.go
@@ -33,34 +33,32 @@ func main() {
 		return
 	}
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	toAddress := common.HexToAddress("0xb349Eba018bFA9d89Da90829629D39668F6653A2")
-	auth := bind.NewKeyedTransactor(privateKey)
-	auth.Nonce = big.NewInt(rand.Int63n(100000000))
-	auth.Value = nil     // in wei
-	auth.GasLimit = uint64(1000000) // in units
-	auth.BlockLimit = uint64(0) // i change the source code, then it can auto figure out if pass zero value
+	toAddress := common.HexToAddress("0xB818715eb048286A608B5E9851877AD7A30a41A0")
+	auth := bind.NewKeyedTransactor(privateKey) // 设置私钥和签名方法
+	auth.Nonce = big.NewInt(rand.Int63n(100000000)) // 也是fisco-bcos的randomid字段，这里按照web3.js计算随机数
+	auth.Value = nil
+	auth.GasLimit = uint64(1000000)
+	auth.BlockLimit = uint64(740) // 区别go-etherneum，似乎是fisco-bcos判断了范围？
 	auth.GasPrice = nil
 	auth.From = fromAddress
-
 	instance, err := hello.NewHello(toAddress, client)
 	if err != nil {
 		log.Fatal("hello.NewHello error ===>", err)
 		return
 	}
-	result, err := instance.Speak(nil)
+	result, err := instance.Speak(nil) // 调用Hello.sol 的 speak方法
 	if err != nil {
 		log.Fatal("instance.Speak error ===>", err)
 		return
 	}
-	fmt.Println("\nfirst speak ===>", string(result[:])) // "bar"
-
-	tx, err := instance.SaySomethingElse(auth, "o(╯□╰)o")
+	fmt.Println("\nfirst speak ===>", string(result[:])) //
+	tx, err := instance.SaySomethingElse(auth, "━((*′д｀)爻(′д｀*))━!!!!") // 调用Hello.sol 的 SaySomethingElse方法
 	if err != nil {
 		log.Fatal("instance.SaySomethingElse", err)
 		return
 	}
 
-	fmt.Printf("\ntx sent: %s", tx.Hash().Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
+	fmt.Printf("tx sent: %s", tx.Hash().Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
 	time.Sleep(3 * time.Second)
 	result, err = instance.Speak(nil)
 	if err != nil {
