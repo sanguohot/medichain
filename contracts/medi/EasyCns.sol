fbb25c61262d2e91f6151110d3e01caf78d43264
evan <281406674@qq.com>
2018-08-23 12:46:14 +0800

    add set event to EasyCns.sol
    Signed-off-by: evan <281406674@qq.com>

--- contracts/medi/EasyCns.sol
+++ contracts/medi/EasyCns.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.4.11;
+import "./ValidateUtil.sol";
 
 //abi info base contract
 contract EasyCns {
     address owner;
     mapping(string => address) nameToAddressMap;
+    
+    event onSet(string name, address addr);
+    
     //constrct function
     function EasyCns() public {
         owner = msg.sender;
@@ -14,7 +18,9 @@ contract EasyCns {
     }
     //set member version
     function set(string name, address addr) public onlyOwner {
+        require(ValidateUtil.stringNotEmpty(name));
         nameToAddressMap[name] = addr;
+        onSet(name, addr);
     }
     //get member version
     function get(string name) constant public returns(address) {
