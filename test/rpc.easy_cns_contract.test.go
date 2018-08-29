b03cf2af70ab97536331f5db830c823b11a9cf16
evan <281406674@qq.com>
2018-08-29 10:34:23 +0800

    test controller and orgsdata ok update config.json
    Signed-off-by: evan <281406674@qq.com>

--- test/rpc.easy_cns_contract.test.go
+++ test/rpc.easy_cns_contract.test.go
@@ -9,13 +9,13 @@ import (
 )
 
 func main() {
-	name := etc.ContractController
+	name := etc.ContractFilesData
 	if err, address := chain.GetAddressFromCns(name); err != nil {
 		log.Fatal(err)
 	}else {
 		fmt.Printf("%s ===> %s\n", name, address.Hex())
 	}
-	if err, hash := chain.SetAddressToCns(name, common.HexToAddress("0xA757d8d58ce9BCFDF74C2a09f716C127F2B86384")); err != nil {
+	if err, hash := chain.SetAddressToCns(name, common.HexToAddress("0xE9348D3180c71aCf42183EFa07F09a84c5Cd2558")); err != nil {
 		log.Fatal(err)
 	}else {
 		fmt.Printf("tx sent: %s\n", hash.Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
