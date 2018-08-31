3d2a4adb6149677b73c4c649f3efdf0a9bd821e1
evan <281406674@qq.com>
2018-08-31 21:56:31 +0800

    测试ok，恢复校验代码继续测试
    Signed-off-by: evan <281406674@qq.com>

--- test/rpc.files_data_contract.test.go
+++ test/rpc.files_data_contract.test.go
@@ -13,20 +13,22 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	//"medichain/etc"
+	"medichain/etc"
 )
 
 func main() {
-	//name := etc.ContractController
-	//err, address := chain.GetAddressFromCns(name)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println("ContractController address ===>", address.Hex())
-	//if err, hash := chain.FilesDataAddSuper(*address); err != nil {
-	//	log.Fatal(err)
-	//}else {
-	//	fmt.Printf("tx sent: %s\n", hash.Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
-	//}
+	name := etc.ContractController
+	err, address := chain.GetAddressFromCns(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("ContractController address ===>", address.Hex())
+	if err, hash := chain.FilesDataAddSuper(*address); err != nil {
+		log.Fatal(err)
+	}else {
+		fmt.Printf("tx sent: %s\n", hash.Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
+	}
+	return
 	//time.Sleep(time.Second * 1)
 	owner, err := chain.FilesDataGetOwner()
 	if err != nil {
