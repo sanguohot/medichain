d7dd8df7a69d3121da99de08fa8de7f6548eccf8
evan <281406674@qq.com>
2018-09-01 18:48:35 +0800

    regen code as pkg error

--- contracts/auth/run.sh
+++ contracts/auth/run.sh
@@ -1,10 +1,11 @@
 array=(Auth)
+pkg=auth
 for data in ${array[@]}  
 do  
     echo "generate ${data} ..."
 	fisco-solc --overwrite --abi ${data}.sol -o build
 	fisco-solc --overwrite --bin ${data}.sol -o build
-	abigen --solc fisco-solc --bin=build/${data}.bin --abi=build/${data}.abi --pkg=medi --type=${data} --out=${data}.go	
+	abigen --solc fisco-solc --bin=build/${data}.bin --abi=build/${data}.abi --pkg=${pkg} --type=${data} --out=${data}.go	
 done
 tree ./
 echo "generate all file successfully!"
