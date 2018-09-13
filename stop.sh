de0f21e875733b9b5faf09d4d3e0ea69b614a749
root <281406674@qq.com>
2018-09-13 18:44:32 +0800

    fix stop.sh not work error

--- stop.sh
+++ stop.sh
@@ -4,7 +4,7 @@ if [ ! $TAG ]; then
   echo "MEDICHAIN_TAG env not set, e.g. 1.0,2.0,latest"
   exit 1
 fi
-IMG=sanguohot/medichain:$TAG
+NAME=medichain
 SERVER_PATH=/opt/medichain
-docker stop ${IMG}
-docker rm ${IMG}
+docker stop ${NAME}
+docker rm ${NAME}
