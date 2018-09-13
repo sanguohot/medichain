4b3231b33bc1f399d8b0e4e5b7847c6f183b0acc
root <281406674@qq.com>
2018-09-13 10:13:27 +0800

    fix not found secret error

--- k8s/build.sh
+++ k8s/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 source config_util
 yaml_array=(\
-medichain-deploy.template.yaml \
+medichain-secret.template.yaml \
 medichain-configmap.template.yaml \
-medichain-service.template.yaml  \
 medichain-pv-keystore.template.yaml \
 medichain-pv-databases.template.yaml \
 medichain-pvc-keystore.template.yaml \
 medichain-pvc-databases.template.yaml \
+medichain-deploy.template.yaml \
+medichain-service.template.yaml  \
 )
 for data in ${yaml_array[@]}
 do
     ./build_core.sh ${data}
 done
-
