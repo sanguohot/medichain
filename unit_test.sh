d929740e5943f281510072b6776df27fda3201f7
root <281406674@qq.com>
2018-09-17 16:55:12 +0800

    add unit_test.sh

---
+++ unit_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+echo "now going to do unit test..."
+export MEDICHAIN_PATH=$(pwd)
+echo "with diretory ${MEDICHAIN_PATH}"
+module_array=(\
+chain \
+datacenter \
+)
+for data in ${module_array[@]}
+do
+	echo "now test ${data} module..."
+    go test -v ./${data}/ -cover=true
+	if [[ $? == *FAIL* ]]; then
+		exit 1
+	fi
+done
+echo "all module test successfully!"
