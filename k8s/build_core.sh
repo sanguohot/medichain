ffb7668af1f97af6e10abbd90da4da37eedbb15c
evan <281406674@qq.com>
2018-09-17 20:08:53 +0800

    k8s: fix config not match configmap error
    Signed-off-by: evan <281406674@qq.com>

--- k8s/build_core.sh
+++ k8s/build_core.sh
@@ -4,7 +4,6 @@ IN_CONFIG_FILE=config_util
 IN_YAML_TEMPLATE_FILE=$1
 OUT_YAML_PATH=".tmp"
 OUT_YAML_FILE=${OUT_YAML_PATH}/${IN_YAML_TEMPLATE_FILE/.template./.}
-
 mkdir -p ${OUT_YAML_PATH}
 
 config=`cat ./${IN_CONFIG_FILE}`
