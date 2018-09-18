dae24a762ebb306d996f4cc33d30ede9b6528b62
evan <281406674@qq.com>
2018-09-18 08:50:26 +0800

    k8s: update configmap to share etc/config.json
    Signed-off-by: evan <281406674@qq.com>

--- k8s/build.sh
+++ k8s/build.sh
@@ -2,7 +2,7 @@
 source config_util
 yaml_array=(\
 medichain-secret.template.yaml \
-medichain-configmap.template.yaml \
+#medichain-configmap.template.yaml \
 medichain-pv-keystore.template.yaml \
 medichain-pv-databases.template.yaml \
 medichain-pvc-keystore.template.yaml \
@@ -10,6 +10,8 @@ medichain-pvc-databases.template.yaml \
 medichain-deploy.template.yaml \
 medichain-service.template.yaml  \
 )
+kubectl -n ${MEDICHAIN_NAMESPACE} create configmap ${MEDICHAIN_NAME}-configmap --from-file=../etc/config.json --dry-run -o yaml > .tmp/${MEDICHAIN_NAME}-configmap.yaml
+echo "build ../etc/config.json to .tmp/${MEDICHAIN_NAME}-configmap.yaml successfully!"
 for data in ${yaml_array[@]}
 do
     ./build_core.sh ${data}
