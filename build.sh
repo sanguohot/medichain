77e5a574fd2cbd0ba8e679a7cfb85f76015ffad1
evan <281406674@qq.com>
2018-09-27 01:53:24 +0000

    build.sh: suport modvendor

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 set -o errexit
 type go >/dev/null 2>&1 || { echo >&2 "go command required but it's not installed.  Aborting."; exit 1; }
 GO11MODULE=on
-go build --ldflags "-linkmode external -extldflags -static" -a -o main -v
+#go build --ldflags "-linkmode external -extldflags -static" -a -o main -v
+GOTEMP=$(mktemp -d) && \
+    GOPATH=$GOTEMP GOPROXY=file://$PWD/modvendor go build --ldflags "-linkmode external -extldflags -static" -a -o main -v && \
+    rm -rf $GOTEMP
